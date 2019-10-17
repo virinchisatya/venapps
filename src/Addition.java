95213e51fa0412db8ff7ae53f797b346272f3c81
DELL <DELL@USER>
2019-10-17 14:17:48 +0530

    trying

---
+++ src/Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=5,b=10;
+		int c=a+b;
+		System.out.println("the value of addition is="+c);
+	}
+
+}
